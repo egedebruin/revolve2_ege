2f8720820c1ad083ebd5ecea96e4c8e86f26acce
Ege <eged@uio.no>
2025-03-05 17:56:13 +0100

    Add environment to test on in random sample, and revert circular parameter

--- eges_projects/evolve_and_learn/genotypes/brain_genotype_simple.py
+++ eges_projects/evolve_and_learn/genotypes/brain_genotype_simple.py
@@ -10,13 +10,6 @@ from genotypes.brain_genotype import BrainGenotype as AbstractBrainGenotype
 
 class BrainGenotype(AbstractBrainGenotype):
 
-    @staticmethod
-    def sin_cos_to_angle(sin_val, cos_val):
-        angle = np.arctan2(sin_val, cos_val)
-        if angle < 0:
-            angle += 2 * np.pi  # Adjust to the range [0, 2π]
-        return angle
-
     def develop_brain(self, body: BodyV1):
         active_hinges = body.find_modules_of_type(ActiveHinge)
 
@@ -25,11 +18,8 @@ class BrainGenotype(AbstractBrainGenotype):
         offsets = []
         for active_hinge in active_hinges:
             amplitudes.append(self.brain[active_hinge.map_uuid][0])
-            phases.append(BrainGenotype.sin_cos_to_angle(
-                self.brain[active_hinge.map_uuid][1] * 2 - 1,
-                self.brain[active_hinge.map_uuid][2] * 2 - 1
-            ))
-            offsets.append(self.brain[active_hinge.map_uuid][3] - 0.5)
+            phases.append(self.brain[active_hinge.map_uuid][1] * 2 * math.pi)
+            offsets.append(self.brain[active_hinge.map_uuid][2] - 0.5)
 
         brain = SineBrain(
             active_hinges=active_hinges,
@@ -45,8 +35,7 @@ class BrainGenotype(AbstractBrainGenotype):
         pbounds = {}
         for key in brain_uuids:
             pbounds['amplitude_' + str(key)] = [0, 1]
-            pbounds['phase_sin_' + str(key)] = [0, 1]
-            pbounds['phase_cos_' + str(key)] = [0, 1]
+            pbounds['phase_' + str(key)] = [0, 1]
             pbounds['offset_' + str(key)] = [0, 1]
         return pbounds
 
@@ -55,9 +44,8 @@ class BrainGenotype(AbstractBrainGenotype):
         next_point = {}
         for key in brain_uuids:
             next_point['amplitude_' + str(key)] = self.brain[key][0]
-            next_point['phase_sin_' + str(key)] = self.brain[key][1]
-            next_point['phase_cos_' + str(key)] = self.brain[key][2]
-            next_point['offset_' + str(key)] = self.brain[key][3]
+            next_point['phase_' + str(key)] = self.brain[key][1]
+            next_point['offset_' + str(key)] = self.brain[key][2]
         return next_point
 
     def next_point_to_brain(self, next_point, brain_uuids):
@@ -65,21 +53,10 @@ class BrainGenotype(AbstractBrainGenotype):
             self.brain[brain_uuid] = np.array(
                 [
                     next_point['amplitude_' + str(brain_uuid)],
-                    next_point['phase_sin_' + str(brain_uuid)],
-                    next_point['phase_cos_' + str(brain_uuid)],
+                    next_point['phase_' + str(brain_uuid)],
                     next_point['offset_' + str(brain_uuid)],
                 ]
             )
-
-    def get_random_next_point(self, rng):
-        brain_uuids = list(self.brain.keys())
-        next_point = {}
-        for key in brain_uuids:
-            next_point['amplitude_' + str(key)] = rng.random()
-            next_point['phase_sin_' + str(key)] = rng.random()
-            next_point['phase_cos_' + str(key)] = rng.random()
-            next_point['offset_' + str(key)] = rng.random()
-        return next_point
 
     def update_values_with_genotype(self, sorted_inherited_experience):
         current_genotype_keys = [str(key) for key in self.brain.keys()]
@@ -101,7 +78,6 @@ class BrainGenotype(AbstractBrainGenotype):
                 if genotype_key not in unique_keys:
                     brain_data = self.brain[uuid.UUID(genotype_key)]
                     values[f'amplitude_{genotype_key}'] = brain_data[0]
-                    values[f'phase_sin_{genotype_key}'] = brain_data[1]
-                    values[f'phase_cos_{genotype_key}'] = brain_data[2]
-                    values[f'offset_{genotype_key}'] = brain_data[3]
+                    values[f'phase_{genotype_key}'] = brain_data[1]
+                    values[f'offset_{genotype_key}'] = brain_data[2]
         return sorted_inherited_experience
--- eges_projects/evolve_and_learn/randomly_sample_grid.py
+++ eges_projects/evolve_and_learn/randomly_sample_grid.py
@@ -4,7 +4,9 @@ import os
 import random
 from argparse import ArgumentParser
 
-from sqlalchemy import Column, Integer, Float, select
+from bayes_opt import BayesianOptimization, acquisition
+from sklearn.gaussian_process.kernels import Matern
+from sqlalchemy import Column, Integer, Float, Boolean, select
 from sqlalchemy.orm import Session, declarative_base
 
 from database_components.generation import Generation
@@ -13,10 +15,10 @@ from database_components.individual import Individual
 from database_components.learn_genotype import LearnGenotype
 from database_components.population import Population
 from evaluator import Evaluator
-import config
 
 from genotypes.body_genotype_direct import CoreGenotype, BodyDeveloper
 from revolve2.experimentation.database import open_database_sqlite, OpenMethod
+from revolve2.experimentation.rng import seed_from_time, make_rng
 
 Base = declarative_base()
 
@@ -26,9 +28,12 @@ class RandomSample(Base):
     genotype_id = Column(Integer)
     repetition = Column(Integer)
     objective_value = Column(Float)
+    length_scale = Column(Float)
+    alpha = Column(Float)
+    kappa = Column(Float)
+    do_random = Column(Boolean)
 
 def main(inherit_samples, environment, repetition):
-    config.ENVIRONMENT = environment
     evaluator = Evaluator(headless=True, num_simulators=1)
     database_name = f"learn-30_controllers-adaptable_survivorselect-newest_parentselect-tournament_inheritsamples-{inherit_samples}_environment-{environment}_{repetition}."
     files = [file for file in os.listdir('results/new_big') if file.startswith(database_name)]
@@ -39,13 +44,13 @@ def main(inherit_samples, environment, repetition):
             'results/new_big/' + file_name, open_method=OpenMethod.OPEN_IF_EXISTS
         )
         dbengine_write = open_database_sqlite(
-            'results/random/' + file_name, open_method=OpenMethod.NOT_EXISTS_AND_CREATE
+            'results/random_grid/' + file_name, open_method=OpenMethod.NOT_EXISTS_AND_CREATE
         )
         Base.metadata.create_all(dbengine_write)
 
         with Session(dbengine) as ses:
             genotypes = ses.execute(
-                select(Genotype._serialized_body, Genotype.id, Generation.generation_index)
+                select(Genotype, Genotype.id, Generation.generation_index, Individual.objective_value)
                 .join_from(Generation, Population, Generation.population_id == Population.id)
                 .join_from(Population, Individual, Population.id == Individual.population_id)
                 .join_from(Individual, Genotype, Individual.genotype_id == Genotype.id)
@@ -54,53 +59,70 @@ def main(inherit_samples, environment, repetition):
 
         # Deduplicate manually, preserving order
         unique_genotypes = {}
-        for serialized_body, genotype_id, generation_index in genotypes:
+        for genotype, genotype_id, generation_index, objective_value in genotypes:
             if genotype_id not in unique_genotypes:
-                unique_genotypes[genotype_id] = (serialized_body, generation_index)
+                unique_genotypes[genotype_id] = (genotype, generation_index, objective_value)
 
         # Convert to a list of tuples
-        genotypes = [(genotype_id, serialized_body, generation_index) for
-                     genotype_id, (serialized_body, generation_index) in unique_genotypes.items()]
+        genotypes = [(genotype_id, genotype, generation_index, objective_value) for
+                     genotype_id, (genotype, generation_index, objective_value) in unique_genotypes.items()]
+
+        genotypes = sorted(genotypes, key=lambda x: x[3], reverse=True)[:10]
 
         with concurrent.futures.ProcessPoolExecutor(
                 max_workers=100
         ) as executor:
             futures = []
-            for genotype_id, serialized_body, generation_index in genotypes:
-                futures.append(executor.submit(sample, evaluator, serialized_body, genotype_id, generation_index))
+            for length_scale in [0.1, 0.2, 0.4, 0.8]:
+                for alpha in [1e-10, 1e-5, 1e-2, 1]:
+                    for kappa in [1, 2, 3]:
+                        for do_random in [True, False]:
+                            for genotype_id, genotype, _, _ in genotypes:
+                                futures.append(executor.submit(sample, evaluator, genotype, genotype_id, length_scale, alpha, kappa, do_random))
 
         for future in futures:
-            samples, genotype_id = future.result()
+            samples, genotype_id, length_scale, alpha, kappa, do_random = future.result()
             for (i, objective_value) in samples:
-                new_data = RandomSample(genotype_id=genotype_id, repetition=i, objective_value=objective_value)
+                new_data = RandomSample(genotype_id=genotype_id, repetition=i, objective_value=objective_value, length_scale=length_scale, alpha=alpha, kappa=kappa, do_random=do_random)
                 with Session(dbengine_write) as session:
                     session.add(new_data)
                     session.commit()
 
-def sample(evaluator, serialized_body, genotype_id, generation_index):
+def sample(evaluator, genotype, genotype_id, length_scale, alpha, kappa, do_random):
     result = []
 
-    body = CoreGenotype(0.0).deserialize(json.loads(serialized_body))
-    body.reverse_phase_function(body.reverse_phase)
-    body_developer = BodyDeveloper(body)
-    body_developer.develop()
-    brain_uuids = body.check_for_brains()
+    rng_seed = seed_from_time() % 2 ** 32
+    rng = make_rng(rng_seed)
+
+    developed_body = genotype.develop_body()
+    brain_uuids = list(genotype.brain.keys())
+
+    optimizer = BayesianOptimization(
+        f=None,
+        pbounds=genotype.get_p_bounds(),
+        allow_duplicate_points=True,
+        acquisition_function=acquisition.UpperConfidenceBound(kappa=kappa)
+    )
+    optimizer.set_gp_params(alpha=alpha)
+    optimizer.set_gp_params(
+        kernel=Matern(nu=5 / 2, length_scale=length_scale, length_scale_bounds="fixed"))
 
     for i in range(30):
-        next_point = {}
-        for key in brain_uuids:
-            next_point['amplitude_' + str(key)] = random.random()
-            next_point['phase_' + str(key)] = random.random()
-            next_point['offset_' + str(key)] = random.random()
+        if i < 5 and do_random:
+            next_point = genotype.get_random_next_point(rng)
+        else:
+            next_point = optimizer.suggest()
+        next_point = dict(sorted(next_point.items()))
 
         new_learn_genotype = LearnGenotype(brain={})
         new_learn_genotype.next_point_to_brain(next_point, brain_uuids)
+        modular_robot = new_learn_genotype.develop(developed_body)
 
-        modular_robot = new_learn_genotype.develop(body_developer.body)
         objective_value = evaluator.evaluate(modular_robot)
         result.append((i, objective_value))
-    print(generation_index)
-    return result, genotype_id
+        optimizer.register(params=next_point, target=objective_value)
+
+    return result, genotype_id, length_scale, alpha, kappa, do_random
 
 if __name__ == "__main__":
     parser = ArgumentParser()
